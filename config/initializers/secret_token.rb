03ca6fbe3bd4268b22ca83784ead783cb347e3ff
Ben Woodall <benwoody@gmail.com>
2011-03-26 15:23:20 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Witwib2::Application.config.secret_token = '3b36f8d8b0eb2214e9d81d4e6343b3089806c0623980ae706227f68da03131b2b2f51513c36d038b4c9113c1188cc17e40486cbc60ab9059cf130d33a5dba089'
