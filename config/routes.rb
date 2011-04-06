ea8fe856d5922488ce175193971081183d095cdd
Ben Woodall <benwoody@gmail.com>
2011-04-06 07:24:30 -0700

    final push for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Witwib2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => "posts#new"
+root :to => "posts#index"
 
   # See how all your routes lay out with "rake routes"
 
